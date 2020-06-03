55455580ec89247582518cc03c71dc00c6ebbd39
alexundar <a.lushnevsky@yandex.by>
2020-06-03 23:02:31 +0300

    Added delete method end ENV variable for database connection

--- storage/news_mongo_storage.py
+++ storage/news_mongo_storage.py
@@ -1,4 +1,5 @@
 import pymongo
+import os
 
 from storage.abstract_news_storage import AbstractNewsStorage
 
@@ -7,15 +8,21 @@ class NewsMongoStorage(AbstractNewsStorage):
     DATABASE_NAME = 'news_aggregator'
     COLLECTION_NAME = 'news'
 
+    os.environ['DB_HOST'] = 'mongodb://localhost:27017/'
+
     def __init__(self):
         super().__init__()
-        self.client = pymongo.MongoClient("mongodb://localhost:27017/")
+        self.client = pymongo.MongoClient(os.environ.get('DB_HOST'))
         self.database = self.client[self.DATABASE_NAME]
         self.news_collection = self.database[self.COLLECTION_NAME]
 
     def store(self, news):
         self.logger.info(f'Inserting {len(news)} news into database')
         self.news_collection.insert_many(news)
+
+    def delete(self):
+        self.logger.info('Deleting news from database')
+        self.news_collection.delete_many({})
 
     def find(self, site=None):
         if site:
