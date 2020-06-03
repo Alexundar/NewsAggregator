8fd75f8d4ab5ff68d88102290d74e877b29a7b2b
alexundar <a.lushnevsky@yandex.by>
2020-06-03 23:04:04 +0300

    Added ENV variable for choosing the way of scrapping

--- async_main.py
+++ async_main.py
@@ -1,6 +1,8 @@
 import asyncio
 import json
 import logging.config
+import os
+import threading
 
 from async_scraper import HrodnaLifeAsyncScraper, TutByAsyncScraper, NavinybyAsyncScraper
 from client import AsyncHTTPClient
@@ -12,23 +14,42 @@ SCRAPERS = {
     'navinyby': NavinybyAsyncScraper
 }
 
+os.environ['SCRAPER'] = 'SEQ'
+
 
 def read_logging_configuration():
     with open('logging_configuration.json') as file:
         return json.loads(file.read())
 
 
+async def async_scrape(db, async_http_client, news):
+    db.delete()
+    for site, Scraper in SCRAPERS.items():
+        scraper = Scraper(async_http_client)
+        news.append(scraper.async_scrape(site))
+    for article in asyncio.as_completed(news):
+        db.store(await article)
+
+
+async def seq_scrape(db, async_http_client, news):
+    db.delete()
+    for site, Scraper in SCRAPERS.items():
+        scraper = Scraper(async_http_client)
+        news.append(await scraper.async_scrape(site))
+    for article in news:
+        db.store(article)
+
+
 async def async_main():
+    threading.Timer(10 * 3600, async_main).start()
     logging.config.dictConfig(read_logging_configuration())
     async_http_client = AsyncHTTPClient()
     db = NewsMongoStorage()
     news = []
-    for site, Scraper in SCRAPERS.items():
-        scraper = Scraper(async_http_client)
-        news.append(scraper.async_scrape(site))
-    news = asyncio.as_completed(news)
-    for piece_of_news in news:
-        db.store(await piece_of_news)
+    if os.environ.get('SCRAPER') == 'SEQ':
+        await seq_scrape(db, async_http_client, news)
+    elif os.environ.get('SCRAPER') == 'ASYNC':
+        await async_scrape(db, async_http_client, news)
 
 
 if __name__ == '__main__':
